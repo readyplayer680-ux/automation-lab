e7ef36ee8dbbd0d01e5bc0c83487e4dd5d1e39b5
rahulprabhas195-collab <rahulprabhas195@gmail.com>
2026-08-17 15:44:32 +0530

    automation.sh for web app changes

---
+++ automation.sh
@@ -0,0 +1,16 @@
+1. Repository creation and deletion
+2. Branch creation  and deletion
+3. Tag creation and deletion
+4. How to add files and commit changes?
+5. How to list commits and check details of commits?
+6. General settings
+7. Branch protections rules
+8. Blocking the direct push to company account.
+9. Webhooks
+10. SSH and GPG keys
+11. Passsword management of Github
+12. Collaborators - permissions
+13. Organizations
+14. Security logs
+15. Developers settings - PAT tokens
+16. Email notifications
